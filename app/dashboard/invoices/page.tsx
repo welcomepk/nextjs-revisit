be00ed7e09886eb69d419c3305e2ec57db16c2ac
pramod_kamble <kamblepramod806@gmail.com>
2024-07-19 17:38:06 +0530

    dev sync

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,4 +1,4 @@
 export default async function Page() {
-    await new Promise(res => setTimeout(res, 2000))
+    // await new Promise(res => setTimeout(res, 2000))
     return <p>Invoices Page</p>
 }
